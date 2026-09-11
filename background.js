4ee04e92a3eea6025e5316cf0aa0f44f692137e6
구윤재 <cstdio.code@gmail.com>
2026-09-11 15:04:08 +0900

    Relay key events to extension and hosted viewer

--- background.js
+++ background.js
@@ -1,11 +1,22 @@
 chrome.runtime.onMessage.addListener((message) => {
   if (!message || message.type !== 'ADOFAI_KEY_RELAY') return;
 
-  chrome.runtime.sendMessage({
+  const forwarded = {
     type: 'ADOFAI_KEY_VIEWER_EVENT',
     kind: message.kind,
     code: message.code,
     repeat: Boolean(message.repeat)
+  };
+
+  // Extension page listeners (chrome-extension://.../index.html)
+  chrome.runtime.sendMessage(forwarded).catch(() => {});
+
+  // Hosted viewer bridge (https://yoonjaekoo.github.io/key/)
+  chrome.tabs.query({}).then(tabs => {
+    for (const tab of tabs) {
+      if (typeof tab.id !== 'number') continue;
+      chrome.tabs.sendMessage(tab.id, forwarded).catch(() => {});
+    }
   }).catch(() => {});
 });
 
